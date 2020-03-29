2dde05edf0bf163f0dd2dfd012d389ef5e55183c
Edik <manukyan.edik11@gmail.com>
2020-03-29 16:56:22 -0700

    feature: Routers were updated to handle Random Profiles path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from '@core/layout/page-not-found';
 import { HomePageComponent } from '@features/home-page';
 import { ProfileDetailComponent } from '@features/profile/profile-detail';
+import { RandomProfilesComponent } from '@features/profile/random-profiles/random-profiles.component';
 
 export const appRoutes: Routes = [
     {
         component: ProfileDetailComponent,
         data: { name: 'profileDetail' },
         path: 'profile'
+    },
+    {
+        component: RandomProfilesComponent,
+        data: { name: 'profiles' },
+        path: 'profiles'
     },
     {
         component: PageNotFoundComponent,
--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,2 +1,2 @@
 export * from '@core/routing/store/routing.actions';
-export * from '@features/profile/store/profile.actions';
+export * from '@features/profile/store/actions/profile.actions';
